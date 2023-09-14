819d41b87801bba640a804330dd10243d4531da8
Eli Miller <elijah.miller@colorado.edu>
2023-09-14 08:48:17 -0600

    Add get_column functionality.

--- my_utils.py
+++ my_utils.py
@@ -1,13 +1,3 @@
-
-# a. get_column() opens the file named file_name and processes it line by line
-# b. for each line
-# i. split the line into an array
-# ii. check to see if the value in the query_column position of the array matches the value
-# stored in the query_value variable
-# iii. when the above condition is met, add the value in the result_column position to an
-# array
-# c. return the array storing the column values
-
 def get_column(file_name, query_column, query_value, result_column):
     with open (file_name, 'r') as f:
         result = []
