50bad0de80ca593bd93982aae76b68d48d53c7d7
Eli Miller <elijah.miller@colorado.edu>
2023-09-14 08:56:23 -0600

    Change results_column to optional param

--- print_fires.py
+++ print_fires.py
@@ -4,6 +4,6 @@ country='United States of America'
 county_column = 0
 fires_column = 3
 file_name = 'Agrofood_co2_emission.csv'
-fires = get_column(file_name, county_column, country, fires_column)
+fires = get_column(file_name, county_column, country, result_column=fires_column)
 
 print(fires)
